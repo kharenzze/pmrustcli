29f323a59f98b1d5161a3e5d7cddb520fcba76cc
Paolo <str237@gmail.com>
2021-07-08 23:36:19 +0200

    WIP home path

--- src/appconfig.rs
+++ src/appconfig.rs
@@ -1,3 +1,4 @@
+use dirs;
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize)]
@@ -7,10 +8,15 @@ pub struct AppConfig {
 
 impl AppConfig {
     pub fn load() -> Self {
+        let home_path = dirs::home_dir();
+        if home_path.is_none() {
+            panic!("Cannot find home path");
+        }
+        let home_path = home_path.unwrap();
+        println!("{}", home_path.to_str().unwrap());
         println!("hi!");
         AppConfig {
             token: "asd".to_string()
         }
     }
 }
-
