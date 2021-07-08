930e1569813717b804bb24fbcc2ca8d1e89a8226
Paolo <str237@gmail.com>
2021-07-08 21:02:29 +0200

    Simple BSON usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use bson::{bson};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct AppConfig {
+    token: String,
+}
+
 fn main() {
     println!("Hello, world!");
+    let a = bson!({
+        "token": "tokenvalue",
+        "tokewen": "tokenval"
+    });
+
+    let parsed: AppConfig = bson::from_bson(a).unwrap();
+    println!("{}", &parsed.token);
 }
