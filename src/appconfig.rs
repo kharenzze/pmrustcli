b486ae0d2b7fa55433cf71473b56edfd056f47a2
Paolo <str237@gmail.com>
2021-07-10 19:06:55 +0200

    Opening file

--- src/appconfig.rs
+++ src/appconfig.rs
@@ -1,4 +1,5 @@
 use dirs;
+use std::fs::File;
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize)]
@@ -7,14 +8,23 @@ pub struct AppConfig {
 }
 
 impl AppConfig {
+    fn empty() -> Self {
+        AppConfig {
+            token: "".to_string()
+        }
+    }
+
     pub fn load() -> Self {
         let home_path = dirs::home_dir();
         if home_path.is_none() {
             panic!("Cannot find home path");
         }
-        let home_path = home_path.unwrap();
-        println!("{}", home_path.to_str().unwrap());
-        println!("hi!");
+        let mut home_path = home_path.unwrap();
+        home_path.push(".pmcli");
+        let file = File::open(home_path);
+        if file.is_err() {
+            return Self::empty()
+        }
         AppConfig {
             token: "asd".to_string()
         }
