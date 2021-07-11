39f67fc1879dd808c91875f06abdde1ffb1041f4
Paolo <str237@gmail.com>
2021-07-11 12:17:49 +0200

    Define app and 'save token' action

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
 mod appconfig;
+
+use seahorse::{App, Context, Command};
+use std::env;
 use appconfig::AppConfig;
 
 fn main() {
-    println!("Hello, world!");
-    let mut conf = AppConfig::load();
-    println!("{}", &conf.token);
-    conf.token = "settingToken".to_string();
-    conf.save();
+    let args: Vec<String> = env::args().collect();
+    let app = App::new(env!("CARGO_PKG_NAME"))
+        .description(env!("CARGO_PKG_DESCRIPTION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .usage("cli [args]")
+        .action(|_| println!("Welcome to pmrustcli"))
+        .command(token_command());
 
-    println!("{}", &conf.token);
+    app.run(args);
 }
+
+fn token_command() -> Command {
+     Command::new("token")
+        .description("set session token")
+        .alias("t")
+        .usage("cli token(t) [token]")
+        .action(token_action)
+}
+
+fn token_action(c: &Context) {
+    let mut conf = AppConfig::load();
+    let token = c.args.get(0).expect("Missing token").to_string();
+    conf.token = token;
+    conf.save();
+    println!("Token saved");
+} 
