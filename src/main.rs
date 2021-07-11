e954d5410b5d7812d9cb650a05b100a58372c512
Paolo <str237@gmail.com>
2021-07-11 11:51:37 +0200

    Storing data

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use appconfig::AppConfig;
 
 fn main() {
     println!("Hello, world!");
-    let conf = AppConfig::load();
+    let mut conf = AppConfig::load();
+    println!("{}", &conf.token);
+    conf.token = "settingToken".to_string();
     conf.save();
 
     println!("{}", &conf.token);
