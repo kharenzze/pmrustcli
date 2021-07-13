b53abd191033033ef202a429f461a4d08b5741ce
Paolo <str237@gmail.com>
2021-07-14 00:13:30 +0200

    get_email

--- src/pm/models/me.rs
+++ src/pm/models/me.rs
@@ -4,12 +4,18 @@ type JSON = serde_json::Value;
 
 pub struct Me(pub JSON); 
 
-impl fmt::Display for Me {
-  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    let email: &str = self.0.get("email")
+impl Me {
+  pub fn get_email(&self) -> &str {
+    self.0.get("email")
       .unwrap()
       .as_str()
-      .unwrap();
+      .unwrap()
+  }
+}
+
+impl fmt::Display for Me {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    let email = self.get_email();
     write!(f, "{}", email)
   }
 }
