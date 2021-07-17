d1b8a711e5b79ffba1609c2ac8e67fcfab917bc6
Paolo <str237@gmail.com>
2021-07-17 13:37:57 +0200

    Print link as well

--- src/pm/models/item.rs
+++ src/pm/models/item.rs
@@ -1,6 +1,7 @@
 use std::fmt;
 use serde_json::{Result as JSON_Result};
 use serde::Deserialize;
+use crate::{PM_BASE};
 
 type JSON = serde_json::Value;
 
@@ -20,6 +21,12 @@ impl SimpleItem {
     }
   }
 
+  fn get_link(&self) -> String {
+    let mut link = PM_BASE!("/office365/app/index/item/").to_string();
+    link.push_str(&self.id.to_string());
+    link
+  }
+
   pub fn from_json(json: JSON) -> JSON_Result<Self> {
     let i: Self = serde_json::from_value(json)?;
     Ok(i)
@@ -28,6 +35,6 @@ impl SimpleItem {
 
 impl fmt::Display for SimpleItem {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    write!(f, "{}", &self.name)
+    write!(f, "{}\t{}", &self.name, &self.get_link())
   }
 }
